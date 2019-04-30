5393261bf6dccda3a4d8243801dbdc29a3fbd6fe
Trevahr Robertson <trevahr@yahoo.com>
2019-04-30 20:06:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+ 
+  names.each_with_index do |value, index|
+    puts "#{index + 1} #{value}"
+  end
+      
 end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
